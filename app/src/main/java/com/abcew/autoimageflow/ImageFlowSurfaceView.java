f5af94ca9004d47db2bf854975406ae766c165f8
zaiyong <zaiyongs@gmail.com>
2016-05-24 10:50:11 +0800

    dd

---
+++ app/src/main/java/com/abcew/autoimageflow/ImageFlowSurfaceView.java
@@ -0,0 +1,23 @@
+package com.abcew.autoimageflow;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+/**
+ * Created by laputan on 16/5/24.
+ */
+public class ImageFlowSurfaceView extends SurfaceView {
+    public ImageFlowSurfaceView(Context context) {
+        super(context);
+    }
+
+    public ImageFlowSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ImageFlowSurfaceView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
